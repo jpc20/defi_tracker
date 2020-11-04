580fb809817f1bdcb31a27820a43e98b0f9d7216
Jack Cullen <jpcullen20@gmail.com>
2020-11-04 13:39:04 -0700

    Add connect model to view

--- defi_app/defi/views.py
+++ defi_app/defi/views.py
@@ -4,4 +4,6 @@ from .models import Project
 
 # Get Projects and display them
 def index(request):
-    return render(request, 'defi/index.html')
+    project_list = Project.objects.order_by('-tvl')[:10]
+    context = {'project_list' : project_list}
+    return render(request, 'defi/index.html', context)
